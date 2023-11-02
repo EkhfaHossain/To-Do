b102c2725ec9a1cf41ff09a48919971fd02bcfa3
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-11-02 17:49:08 +0600

    Added Mantine UI with NextJS

---
+++ todo-list/src/pages/index.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Home.module.css'
+import { Button } from '@mantine/core'
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="filled"color="pink"> Hello </Button>
+      <Button fullWidth>Full width button</Button>
+    </div>  
+  )
+}
